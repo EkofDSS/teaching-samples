c3408d4f8c903b956f499542a418525381d7910d
Vladimir Filipovic <vladofilipovic@hotmail.com>
2024-12-17 22:05:24 +0100

    dump

--- 06-c#-blazor-actors-db-supabase/01-complete-system-supabase/dump-supabase-database.cmd
+++ 06-c#-blazor-actors-db-supabase/01-complete-system-supabase/dump-supabase-database.cmd
@@ -1,1 +1,1 @@
-pg_dump --inserts --column-inserts --username=vladofilipovic@hotmail.com --password=s_x1xxxxxxx --host=hetwconijbntskcjnptc.supabase.co --port=5432 Movies > database-dump.sql
+pg_dump --inserts --column-inserts --username=vladofilipovic@hotmail.com --password=x_X1xxxxxxx --host=kfexoiikhweqcqygytez.supabase.co --port=5432 Movies > database-dump.sql
